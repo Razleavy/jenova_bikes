97fcf459b8934d4634a714fb4fb09b45bcf49299
Razleavy <razleavy@gmail.com>
2020-08-04 20:37:41 +0100

    Hero is now componentised

--- src/app/shared/hero/hero.component.ts
+++ src/app/shared/hero/hero.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+import { Hero } from 'src/app/models/hero/hero.model';
+import { DomSanitizer, SafeStyle } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-hero',
@@ -7,7 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroComponent implements OnInit {
 
-  constructor() { }
+  @Input() hero: Hero;
+
+  get heading(): string {
+    return this.hero.heading;
+  }
+
+  get subheading(): string {
+    return this.hero.subHeading
+  }
+
+  get backgroundImage(): SafeStyle {
+    return this._sanitizer.bypassSecurityTrustStyle(`linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url(${this.hero.backgroundImage})`);
+  }
+
+  constructor(private _sanitizer: DomSanitizer) { }
 
   ngOnInit(): void {
   }
