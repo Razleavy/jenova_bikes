9ad65b0e60ebf728abc32d32c6e00186a162f3bd
Razleavy <razleavy@gmail.com>
2020-08-30 15:09:28 +0100

    Added basic 404 page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [{ path: '', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) }];
+const routes: Routes = [
+  { path: '', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
+  { path: '404', loadChildren: () => import('./pages/notfound/notfound.module').then(m => m.NotfoundModule) },
+  { path: '**', redirectTo: '404' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
