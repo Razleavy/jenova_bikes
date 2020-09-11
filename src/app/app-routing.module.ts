1135e3bd0ca612fa1cf08f05206f14338df765a5
Razleavy <razleavy@gmail.com>
2020-09-11 21:34:17 +0100

    Routing and logic updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { Paths } from './models/enums/paths';
-
+import { PathResolveService } from './pages/notfound/resolvers/path.resolver';
 
 const routes: Routes = [
   { path: '', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
-  { path: Paths.PAGE_NOT_FOUND, loadChildren: () => import('./pages/notfound/notfound.module').then(m => m.NotfoundModule) },
-  { path: '**', redirectTo: Paths.PAGE_NOT_FOUND }
+  { path: '**', loadChildren: () => import('./pages/notfound/notfound.module').then(m => m.NotfoundModule), resolve: { path: PathResolveService } },
+
 ];
 
 @NgModule({
